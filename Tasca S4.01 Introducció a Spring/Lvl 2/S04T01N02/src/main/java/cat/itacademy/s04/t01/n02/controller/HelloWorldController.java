0af1788840023b7ad03dd892bb1c6ebdcc32201a
Ainoaran <ainoa.aam@gmail.com>
2024-11-06 15:20:31 +0100

    S4 01

--- Tasca S4.01 Introducció a Spring/Lvl 2/S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
+++ Tasca S4.01 Introducció a Spring/Lvl 2/S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
     public String hello(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name){
-        return "Hi, " + name + ". You're running a Maeven project. ";
+        return "Hi, " + name + ". You're running a Gradle project. ";
 
     }
 
@@ -16,7 +16,7 @@ public class HelloWorldController {
 
     @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
     public String hello2(@PathVariable(value = "name", required = false) String name){
-        return "Hi, " + (name != null ? name : "UNKNOWN")  + ". You're running a Maeven project. ";
+        return "Hi, " + (name != null ? name : "UNKNOWN")  + ". You're running a Gradle project. ";
     }
 
     //http://localhost:9001/HelloWorld2
