12b64bc7af531d0e9a759eaf57509c4813252c4d
Ainoa <ainoa.aam@gmail.com>
2024-11-18 13:08:16 +0100

    Update GlobalExceptionHandler.java

--- Tasca S4.02 Api Rest amb Spring boot/Lvl 1/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/exception/GlobalExceptionHandler.java
+++ Tasca S4.02 Api Rest amb Spring boot/Lvl 1/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/exception/GlobalExceptionHandler.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    // Manejo de excepción para frutas duplicadas
+   
     @ExceptionHandler(DuplicateFruitException.class)
     public ResponseEntity<String> handleDuplicatedFruitException(DuplicateFruitException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
-    // Manejo de excepción para fruta no encontrada
+   
     @ExceptionHandler(FruitNotFoundException.class)
     public ResponseEntity<String> handleFruitNotFoundException(FruitNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    //Manejo de exception para error de conexión
+    
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e){
         return new ResponseEntity<>("Unexpected error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
